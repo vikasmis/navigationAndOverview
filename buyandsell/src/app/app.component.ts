6b0c43a9e356f876d99068e0fd4f3e2cea194269
Mishra <vikash.a.mishra@sogeti.com>
2020-10-07 13:57:49 +0200

    Add Fix

--- buyandsell/src/app/app.component.ts
+++ buyandsell/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { childlists } from './childlists'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
-  title = 'BuyAndSell';
+  title = 'This is my family details';
+  childlists = childlists;
+  
+  alertButton() {
+    window.alert('The product has been shared!');
+  }
 }
---
+++ buyandsell/src/app/childlists.ts
@@ -0,0 +1,5 @@
+export const childlists = [
+    { name: 'vikash', age: '34y', details: 'father' },
+    { name: 'charu', age: '31y', details: 'mother' },
+    { name: 'charvik', age: '8m', details: 'son' }
+]
