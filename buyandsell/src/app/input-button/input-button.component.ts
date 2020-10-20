99ff783eb433b1e9c578c6e9200a6f788c9346a3
Mishra <vikash.a.mishra@sogeti.com>
2020-10-20 16:01:04 +0200

    changes as per form

--- buyandsell/src/app/input-button/input-button.component.ts
+++ buyandsell/src/app/input-button/input-button.component.ts
@@ -15,9 +15,39 @@ export class inputButton {
     firstName = '';
     familyDetails = '';
     membersAge = '';
+    msgClickval=''
+    // values='';
+    // valuess='';
+    // valuesss='';
+    UserName='';
+    UserAge='';
+    UserDescription=''
     clickFunction() {
 
         alert("clicked me!");
 
     }
+    
+    msgClick(){
+     this.msgClickval='my value is changed'
+
+    }
+    keyEvent(value: string) {
+     this.UserName = value +"  "
+    }
+    keyEvent1(value: string) {
+        this.UserAge = value +"  "
+       }
+       keyEvent2(value: string) {
+        this.UserDescription = value +"  "
+       }
+    // ngName( userName : string){
+    //     this.UserName=userName +''
+    // }
+    // ngAge( age : string){
+    //     this.UserAge=age +''
+    // }
+    // ngDetails( detail : string){
+    //     this.UserDescription=detail +''
+    // }
 }
