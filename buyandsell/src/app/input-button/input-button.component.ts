900a2e9ac9945be3b164be969f2fd9c56c3fe178
Mishra <vikash.a.mishra@sogeti.com>
2020-10-22 15:21:06 +0200

    this loal storage fix

--- buyandsell/src/app/input-button/input-button.component.ts
+++ buyandsell/src/app/input-button/input-button.component.ts
@@ -15,39 +15,76 @@ export class inputButton {
     firstName = '';
     familyDetails = '';
     membersAge = '';
-    msgClickval=''
-    // values='';
-    // valuess='';
-    // valuesss='';
-    UserName='';
-    UserAge='';
-    UserDescription=''
+    msgClickval = ''
+    UserName = [];
+    UserAge = [];
+    UserDescription = [];
+    arrayUserName = [];
+    arrayUserAge = [];
+    arrayUserDes = [];
+    // x=''
+    // heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
     clickFunction() {
 
         alert("clicked me!");
 
     }
-    
-    msgClick(){
-     this.msgClickval='my value is changed'
+
+    msgClick() {
+        this.msgClickval = 'my value is changed'
 
     }
     keyEvent(value: string) {
-     this.UserName = value +"  "
+        if (value) {
+            var sideBar = JSON.parse(localStorage.getItem('username'));
+            if (sideBar) {
+                this.arrayUserName = sideBar;
+                console.log(this.arrayUserName)
+                this.arrayUserName.push(value);
+                localStorage.setItem("username", JSON.stringify(this.arrayUserName))
+            }
+            else {
+                this.UserName.push(value)
+                localStorage.setItem("username", JSON.stringify(this.UserName))
+            }
+        }
+
     }
+
     keyEvent1(value: string) {
-        this.UserAge = value +"  "
-       }
-       keyEvent2(value: string) {
-        this.UserDescription = value +"  "
-       }
-    // ngName( userName : string){
-    //     this.UserName=userName +''
-    // }
-    // ngAge( age : string){
-    //     this.UserAge=age +''
-    // }
-    // ngDetails( detail : string){
-    //     this.UserDescription=detail +''
-    // }
+        if (value) {
+            var sideBar = JSON.parse(localStorage.getItem('Age'));
+            if (sideBar) {
+                this.arrayUserAge = sideBar;
+                console.log(this.arrayUserAge)
+                this.arrayUserAge.push(value);
+                localStorage.setItem("Age", JSON.stringify(this.arrayUserAge))
+            }
+            else {
+                this.UserAge.push(value);
+                localStorage.setItem("Age", JSON.stringify(this.UserName))
+            }
+        }
+    }
+    keyEvent2(value: string) {
+        if (value) {
+            var sideBar = JSON.parse(localStorage.getItem('UserDescription'));
+            if (sideBar) {
+                this.UserDescription.push(value);
+
+                this.arrayUserDes = sideBar;
+                console.log(this.arrayUserDes)
+                this.arrayUserDes.push(value);
+                localStorage.setItem("UserDescription", JSON.stringify(this.arrayUserDes))
+            }
+            else {
+                this.UserAge.push(value);
+                localStorage.setItem("UserDescription", JSON.stringify(this.UserDescription))
+            }
+        }
+    }
+
+    userNamesText: string = localStorage.getItem('username') ? JSON.parse(localStorage.getItem('username')) : [];
+    usersAgeNum: string = localStorage.getItem('Age') ? JSON.parse(localStorage.getItem('Age')) : [];
+    usersDesText: string = localStorage.getItem('UserDescription') ? JSON.parse(localStorage.getItem('UserDescription')) : [];
 }
